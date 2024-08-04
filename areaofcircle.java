6d4ebd10fe3f97ef0261913324ce5bd12caf05e5
dharaniyarravula <147791784+dharaniyarravula@users.noreply.github.com>
2024-08-04 18:46:26 +0530

    Create areaofcircle.java

---
+++ areaofcircle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+    class areaofcircle
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+      double area,r;
+       r=sc.nextDouble();
+       area=4*3.14*r*r;
+       System.out.println("areaofcircle"+area);
+        sc.close();
+        
+    }
+}
